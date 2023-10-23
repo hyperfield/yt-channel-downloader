de2d64f98f92583fd5ae1086d41b54fb2a41bfdd
hyperfield <8313950+hyperfield@users.noreply.github.com>
2023-10-23 20:53:02 +0200

    Added SSL environment setup for Windows.

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 
 # This Python file uses the following encoding: utf-8
 import sys
-from classes.mainwindow import MainWindow
-from PySide6.QtWidgets import QApplication
+import certifi
+import os
+os.environ['SSL_CERT_FILE'] = certifi.where()
 
 # Important:
 # You need to run the following command to generate the ui_form.py file
