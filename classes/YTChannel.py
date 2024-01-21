ebd4f922c180d54ac338b3647850ac67238e4e0d
hyperfield <8313950+hyperfield@users.noreply.github.com>
2024-01-21 20:02:13 +0100

    Started adding docstrings descriptions for functions. Download by single video URL. Updated requirements.txt to include pytube. Updated the README. Minor improvements.

--- classes/YTChannel.py
+++ classes/YTChannel.py
@@ -1,4 +1,12 @@
+# Author: hyperfield
+# Email: info@quicknode.net
+# Date: October 13, 2023
+# Project: YT Channel Downloader
+# Description: This module contains the classes YTChannel.
+# License: MIT License
+
 import scrapetube
+from pytube import YouTube
 from urllib import request, error
 
 from .constants import KEYWORD_LEN, OFFSET_TO_CHANNEL_ID
@@ -7,6 +15,11 @@ from .constants import KEYWORD_LEN, OFFSET_TO_CHANNEL_ID
 class YTChannel:
     def __init__(self):
         self.channelId = ""
+        self.base_video_url = 'https://www.youtube.com/watch?v='
+        self.video_titles_links = []
+
+    def is_video_url(self, url):
+        return 'youtube.com/watch?v=' in url
 
     def get_channel_id(self, url):
         if "channel/" in url:
@@ -36,12 +49,16 @@ class YTChannel:
             print(e.__dict__)
             raise ValueError
 
-    def get_all_video_in_channel(self, channel_id):
-        base_video_url = 'https://www.youtube.com/watch?v='
-        video_titles_links = []
+    def get_all_videos_in_channel(self, channel_id):
         chan_video_entries = scrapetube.get_channel(channel_id)
         for entry in chan_video_entries:
             vid_title = entry['title']['runs'][0]['text']
-            video_url = base_video_url + entry['videoId']
-            video_titles_links.append([vid_title, video_url])
-        return video_titles_links
+            video_url = self.base_video_url + entry['videoId']
+            self.video_titles_links.append([vid_title, video_url])
+        return self.video_titles_links
+
+    def get_single_video(self, video_url):
+        yt = YouTube(video_url)
+        vid_title = yt.title
+        self.video_titles_links.append([vid_title, video_url])
+        return self.video_titles_links
